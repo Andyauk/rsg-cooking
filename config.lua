852f68a757bb2d37be5bb82d14fe20443f4105a3
RexshackGaming <rexshackuk@gmail.com>
2023-11-12 17:16:13 +0000

    simplify cooking again

--- config.lua
+++ config.lua
@@ -32,178 +32,23 @@ Config.CampfireProps = {
 
 Config.Recipes = {
 
-    -- Pescados
     {
         category = "Fish",
         cooktime = 5000,
         ingredients = { 
-            [1] = { item = "provision_fish_meat", amount = 1 },
+            [1] = { item = "raw_fish", amount = 1 },
         },
         receive = "cooked_fish",
         giveamount = 1
     },
-
-    -- Meats
     {
         category = "Meats",
         cooktime = 5000,
         ingredients = { 
-            [1] = { item = "provision_meat_game", amount = 1 },
+            [1] = { item = "raw_meat", amount = 1 },
         },
-        receive = "consumable_meat_game_cooked",
+        receive = "cooked_meat",
         giveamount = 1
     },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "big_game_meat", amount = 1 },
-        },
-        receive = "big_game_meat_cooked",
-        giveamount = 1
-    },
-
-    ---------------------------------------------------
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_plump_bird", amount = 1 },
-        },
-        receive = "consumable_meat_plump_bird_cooked",
-        giveamount = 1
-    },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_mature_venison", amount = 1 },
-        },
-        receive = "consumable_meat_mature_venison_cooked",
-        giveamount = 1
-    },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_stringy", amount = 1 },
-        },
-        receive = "consumable_meat_stringy_cooked",
-        giveamount = 1
-    },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_herptile", amount = 1 },
-        },
-        receive = "consumable_meat_herptile_cooked",
-        giveamount = 1
-    },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_tender_pork", amount = 1 },
-        },
-        receive = "consumable_meat_tender_pork_cooked",
-        giveamount = 1
-    },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_gristly_mutton", amount = 1 },
-        },
-        receive = "consumable_meat_gristly_mutton_cooked",
-        giveamount = 1
-    },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_gamey_bird", amount = 1 },
-        },
-        receive = "consumable_meat_gamey_bird_cooked",
-        giveamount = 1
-    },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_prime_beef", amount = 1 },
-        },
-        receive = "consumable_meat_prime_beef_cooked",
-        giveamount = 1
-    },
-    {
-        category = "Meats",
-        cooktime = 5000,
-        ingredients = { 
-            [1] = { item = "provision_meat_exotic_bird", amount = 1 },
-        },
-        receive = "consumable_meat_exotic_bird_cooked",
-        giveamount = 1
-    },
-    --------------------------------------------------------------
-
-    -- Baits ------ rsg-hunting support
-    -- {
-    --     category = "Baits",
-    --     cooktime = 5000,
-    --     ingredients = { 
-    --         [1] = { item = "corn", amount = 1 },
-    --         [2] = { item = "carrot", amount = 1 },
-    --     },
-    --     receive = "consumable_herbivore_bait",
-    --     giveamount = 1
-    -- },
-    -- {
-    --     category = "Baits",
-    --     cooktime = 5000,
-    --     ingredients = { 
-    --         [1] = { item = "corn", amount = 1 },
-    --         [2] = { item = "carrot", amount = 1 },
-    --         [3] = { item = "sugar",         amount = 1 },
-    --         [4] = { item = "tomato",    amount = 1 },
-    --         [5] = { item = "broccoli",         amount = 1 },
-    --         [6] = { item = "potato",    amount = 1 },
-    --     },
-    --     receive = "consumable_potent_herbivore_bait",
-    --     giveamount = 1
-    -- },
-    -- {
-    --     category = "Baits",
-    --     cooktime = 5000,
-    --     ingredients = { 
-    --         [1] = { item = "provision_meat_game", amount = 1 },
-    --         [2] = { item = "big_game_meat", amount = 1 },
-    --         [3] = { item = "provision_fish_meat", amount = 1},
-    --     },
-    --     receive = "consumable_predator_bait",
-    --     giveamount = 1
-    -- },
-    -- {
-    --     category = "Baits",
-    --     cooktime = 5000,
-    --     ingredients = { 
-    --         [1] = { item = "big_game_meat",                     amount = 1 },
-    --         [2] = { item = "provision_meat_plump_bird",         amount = 1 },
-    --         [3] = { item = "provision_meat_mature_venison",     amount = 1 },
-    --         [4] = { item = "provision_meat_stringy",            amount = 1 },
-    --         [5] = { item = "provision_meat_herptile",           amount = 1 },
-    --         [6] = { item = "provision_meat_tender_pork",        amount = 1 },
-    --         [7] = { item = "provision_meat_gristly_mutton",     amount = 1 },
-    --         [8] = { item = "provision_meat_gamey_bird",         amount = 1 },
-    --         [9] = { item = "provision_meat_prime_beef",         amount = 1 },
-    --         [10] = { item = "provision_meat_exotic_bird",       amount = 1 },
-    --     },
-    --     receive = "consumable_potent_predator_bait",
-    --     giveamount = 1
-    -- },
-    
- 
     
 }
-
-
